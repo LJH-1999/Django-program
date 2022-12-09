42140067fad71ce5114dfa2dc70c4aa3e7cfd5d7
LIU  JIAHAO <1059723379@qq.com>
2022-12-09 17:31:39 +0800

    modify menu, playground

--- acapp/settings.py
+++ acapp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)85j&pj$j_bib^uixirw^^wxti@^#x@k_3yjv6s&kb^w6h@hf&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8.134.129.207"]
+ALLOWED_HOSTS = ["8.134.129.207", "app4189.acapp.acwing.com.cn"]
 
 
 # Application definition
