8205b9b9ff64f19243ce932713aae02b10e6a1c9
LIU  JIAHAO <1059723379@qq.com>
2022-12-10 16:05:15 +0800

    s

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,7 +1,8 @@
 export class AcGame {
-    constructor(id) {
+    constructor(id, AcWingOS) {
         this.id = id;
         this.$ac_game = $('#' + id);
+        this.$AcWingOS = AcWingOS;
         this.menu = new AcGameMenu(this);
         this.playground = new AcGamePlayground(this);
 
--- game/urls/settings/index.py
+++ game/urls/settings/index.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from game.views.settings.getinfo import getinfo
+
 
 urlpatterns = [
+    path("getinfo/", getinfo, name="settings_getinfo"),
+
 
 ]
