a328979624dd61bff41316ed1571ca0af5bdc69f
LIU  JIAHAO <1059723379@qq.com>
2022-12-17 17:29:53 +0800

    add login layer

--- game/static/js/src/settings/zbase.js
+++ game/static/js/src/settings/zbase.js
@@ -7,6 +7,44 @@ class Settings {
         }
         this.username = "";
         this.photo = "";
+        this.$settings = $(`
+<div class="ac-game-settings">
+    <div class="ac-game-settings-login">
+        <div class="ac-game-settings-title">
+            登录
+        </div>
+        <div class="ac-game-settings-username">
+            <div class="ac-game-settings-item">
+                <input type="text" placeholder="用户名">
+            </div>
+        </div>
+        <div class="ac-game-settings-password">
+            <div class="ac-game-settings-item">
+                <input type="password" placeholder="密码">
+            </div>
+        </div>
+        <div class="ac-game-settings-submit">
+            <div class="ac-game-settings-item">
+                <button>登录</button>
+            </div>
+        </div>
+        <div class="ac-game-settings-error-messages">
+            用户名密码错误
+        </div>
+        <div class="ac-game-settings-option">
+            注册
+        </div>
+    </div>
+    <div class="ac-game-settings-register">
+    </div>
+</div>
+`);
+        this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login.hide();
+        this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register.hide();
+
+        this.root.$ac_game.append(this.$settings);
         this.start();
     }
 
@@ -15,9 +53,13 @@ class Settings {
     }
 
     register() { //打开注册界面
+        this.$login.hide();
+        this.$register.show();
     }
 
     login() {//打开登录界面
+        this.$register.hide();
+        this.$login.show();
     }
 
     getinfo() {//从服务器获取用户信息
@@ -44,8 +86,10 @@ class Settings {
     }
 
     hide() {
+        this.$settings.hide();
     }
 
     show() {
+        this.$settings.show();
     }
 }
